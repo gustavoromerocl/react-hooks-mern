29c537d6f8381fc4afa7f3a4d9c6007bfd4fd4c8
romerodevelopments <gandresrp@gmail.com>
2022-11-26 08:21:51 -0300

    profundizando hooks generales - useCounter customHook

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HooksApp } from './HooksApp'
+// import { CounterApp } from './01-useState/CounterApp'
+// import { HooksApp } from './HooksApp'
+import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HooksApp />
+    <CounterWithCustomHook />
   </React.StrictMode>
 )
